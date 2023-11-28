11fff421842ad3457bec9c2992904bd8ce994600
Kevin Alexa <kevinalexa20@gmail.com>
2023-11-28 14:53:52 +0700

    Setup Appwrite database

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const String databaseID = "65659ba1a5c0101a3a5d";
+  static const String projectID = "656549d10c92c8d38a20656549d10c92c8d38a20";
+  static const String endPoint = "http://localhost:80/";
+}
